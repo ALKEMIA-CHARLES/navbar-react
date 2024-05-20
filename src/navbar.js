abc6900394118fc2a46a56e529cdfdc6d7854078
ALKEMIA-CHARLES <charlesmtawalijr@gmail.com>
2024-05-20 12:08:39 +0300

    done

---
+++ src/navbar.js
@@ -0,0 +1,28 @@
+import { useImperativeHandle } from "react"
+import {Link, useMatch, useResolvedPath} from "react-router-dom"
+
+export default function Navbar() {
+    return (
+     <nav className="nav">
+        <Link to="/" className="site-title">
+            Tunzaa
+            </Link>
+        <ul>
+            <CustomLink to="/pricing">Pricing</CustomLink>
+            <CustomLink to="/about">about</CustomLink>
+        </ul>
+    </nav>
+    )
+}
+
+function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end:true })
+    return (
+        <li className= {isActive ? "active" : ""}>
+            <Link to={to} {...props}>
+                {children}
+            </Link>
+        </li>
+    )
+}
